80ce2bcce7f03fa5f0e7515f14de9d1400d3ebe5
Andrew Palmer <andrew.pa@outlook.com>
2025-05-24 13:23:33 -0700

    add toaster

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono, Pacifico } from "next/font/google";
 import "./globals.css";
 import { Provider } from "@/components/ui/provider";
+import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = Geist({
     variable: "--font-geist-sans",
@@ -35,7 +36,10 @@ export default function RootLayout({
             <body
                 className={`${geistSans.variable} ${geistMono.variable} ${pacifico.variable}`}
             >
-                <Provider>{children}</Provider>
+                <Provider>
+                    {children}
+                    <Toaster/>
+                </Provider>
             </body>
         </html>
     );
